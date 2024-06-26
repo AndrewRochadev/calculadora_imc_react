50bc45d5192ee19885742b669c51333c49fe30cd
Andrew <andrew.pablo66@gmail.com>
2024-06-25 22:00:26 -0300

    commit inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import Calculadora from './components/Calculadora/Index'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1>Calculadora de IMC</h1>
+    <>
+    <Calculadora />
     </>
+    <footer>
+    <h2>O que é IMC?</h2>
+    <p>O Índice de Massa Corporal (IMC) é uma ferramenta que oferece uma visão geral da relação entre peso e altura de uma pessoa. No entanto, é crucial compreender que a saúde é influenciada por vários fatores, e o IMC é apenas um dos indicadores.</p>
+    <h2>Interpretação do IMC</h2>
+    <p>O IMC é classificado em diferentes categorias, cada uma indicando uma faixa geral de peso. Lembre-se de que o IMC é apenas uma medida inicial e não leva em conta todos os fatores individuais de saúde. As categorias são:</p>
+    <ul>
+      <li>Abaixo de 18,5: Peso abaixo do normal.</li>
+      <li>18,5 a 24,9: Peso considerado na faixa saudável.</li>
+      <li>25 a 29,9: Peso na faixa do sobrepeso.</li>
+      <li>30 a 34,9: Peso na faixa da obesidade grau 1.</li>
+      <li>35 a 39,9: Peso na faixa da obesidade grau 2.</li>
+      <li>Acima de 40: Peso na faixa da obesidade grau 3.</li>
+    </ul>
+    </footer>
+    </>
+
   )
-}
+};
 
 export default App
---
+++ src/components/Calculadora/Index.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+import styles from './Calculadora.module.css'
+
+const Calculadora = () => {
+    const [Altura, setAltura] = useState(0);
+    const [Peso, setPeso] = useState(0);
+
+    const mostraResultado = () => {
+        const calculo = Peso / (Altura * Altura);
+        const resultado = calculo.toFixed(2);
+
+        if (resultado <= 18.5) {
+            return (
+                <h2>Seu IMC é: {resultado} <br /> Peso abaixo do normal.</h2>
+            );
+        } else if (resultado > 18.5 && resultado < 24.99) {
+            return (
+                <h2>Seu IMC é: {resultado} <br /> Peso considerado na faixa saudável.</h2>
+            );
+        } else if (resultado >= 24 && resultado < 29.99) {
+            return (
+                <h2>Seu IMC é: {resultado} <br /> Peso na faixa do sobrepeso.</h2>
+            );
+        } else if (resultado >= 30 && resultado < 34.99) {
+            return (
+                <h2>Seu IMC é: {resultado} <br /> Peso na faixa da obesidade grau 1.</h2>
+            );
+        } else if (resultado >= 35 && resultado < 39.99) {
+            return (
+                <h2>Seu IMC é: {resultado} <br /> Peso na faixa da obesidade grau 2.</h2>
+            );
+        } else if (resultado >= 40) {
+            return (
+                <h2>Seu IMC é: {resultado} <br /> Peso na faixa da obesidade grau 3.</h2>
+            );
+        }
+    };
+
+    return (
+        <form>
+            <h2>Seu peso:</h2> <br />
+            <input
+                type="number"
+                placeholder="48.9"
+                onChange={evento => setPeso(parseFloat(evento.target.value))}
+            />
+            <h2>Sua altura:</h2> <br />
+            <input
+                type="number"
+                placeholder="1.67"
+                onChange={evento => setAltura(parseFloat(evento.target.value))}
+            />
+            {mostraResultado()}
+        </form>
+    );
+}
+
+export default Calculadora;
